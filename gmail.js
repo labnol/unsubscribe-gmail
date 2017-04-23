df79037e775bff04c3ae2417e40751cf1c5410c1
Amit Agarwal <amit@labnol.org>
2017-04-23 23:08:19 +0530

    Switched to Google Script
    Switched from client side jQuery based addon to server side Google Apps script

---
+++ gmail.js
@@ -0,0 +1,22 @@
+function main() {
+
+  var label = GmailApp.getUserLabelByName("Unsubscribe");
+  var threads = label.getThreads();
+
+  threads.forEach(function(thread) {
+
+    var message = thread.getMessages()[0];
+    var value = message.getRawContent()
+                       .match(/^List-Unsubscribe: ((.|\r\n\s)+)\r\n/m)[1];
+
+    if (value) {
+      var url = value.match(/<(https?:\/\/[^>]+)>/)[1];
+      if (url) {
+        var status = UrlFetchApp.fetch(url).getResponseCode();
+        Logger.log("Unsubscribe " + status + " " + url);
+      }
+    }
+
+    thread.removeLabel(label);
+  });
+}
